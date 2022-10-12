893525532d4bff0a6431bdee83ef8b8043be6e3b
George Makris <gm.georgemakris@gmail.com>
2022-10-13 00:27:43 +0300

    Initial commit and upload of basic script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bulk archive teams
+---
+# Background
+---
+For the time being in the Teams admin center, there is not way to filter out or batch select teams by archival state. Due to the demand by some administrators to be able to bulk archive the teams of choice, this script was create to cover that demand for the time
+being until this functionality is added in the teams admin center.
+
+# Disclaimer
+---
+This is not an official script developed by microsoft. It is a custom script developed by the owner of this repository and has not been tested in anyway for certification and official use. The script is used at own risk and the developer is not responsible for any 
+adverse effects it might cause.
+
---
+++ archiveTeamsAndSpoSites.ps1
@@ -0,0 +1,27 @@
+﻿# Prerequisite modules.
+# Skip if already installed
+
+Install-Module MicrosoftTeams
+Install-Module ExchangeOnlineManagement
+Install-Module Microsoft.Online.SharePoint.PowerShell
+
+$adminCredential = Get-Credential;
+$SpoAdminSiteUrl = "https://makrisaviation-admin.sharepoint.com/"
+
+Connect-MicrosoftTeams -Credential $adminCredential
+Connect-ExchangeOnline -Credential $adminCredential
+Connect-SPOService -url $SpoAdminSiteUrl
+
+# Collecting Teams to be archived
+Write-Host "Enter title of team or common title section of teams:"
+
+$teamsTitle = Read-Host;
+
+$teamsToArchive = Get-Team -DisplayName $teamsTitle -Archived $False | select GroupId;
+
+# Archiving Teams
+foreach($team in $teamsToArchive) {
+    echo "Archiving team: $team.GroupId"
+    Set-TeamArchivedState $team.GroupId -Archived $True -SetSpoSiteReadOnlyForMembers $True
+    Set-SPOSite -Identity (Get-UnifiedGroup -Identity $team.GroupId).SharePointSiteUrl -LockState ReadOnly
+    }
