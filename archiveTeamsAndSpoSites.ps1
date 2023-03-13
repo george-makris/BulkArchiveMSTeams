ee580600f3836015724ad39d209e6afd7e72c023
George Makris <g.makris96@yahoo.com>
2023-03-13 22:01:40 +0200

    Generalising domain for sharepoint and correcting readme.md

--- archiveTeamsAndSpoSites.ps1
+++ archiveTeamsAndSpoSites.ps1
@@ -5,9 +5,11 @@ Install-Module MicrosoftTeams
 Install-Module ExchangeOnlineManagement
 Install-Module Microsoft.Online.SharePoint.PowerShell
 
+#Retrieve admin credentials and SharePoint admin site URL
 $adminCredential = Get-Credential;
-$SpoAdminSiteUrl = "https://makrisaviation-admin.sharepoint.com/"
+$SpoAdminSiteUrl = "https://TenantDomainName-admin.sharepoint.com/"
 
+#Connect to modules using admin credentials
 Connect-MicrosoftTeams -Credential $adminCredential
 Connect-ExchangeOnline -Credential $adminCredential
 Connect-SPOService -url $SpoAdminSiteUrl
@@ -19,7 +21,7 @@ $teamsTitle = Read-Host;
 
 $teamsToArchive = Get-Team -DisplayName $teamsTitle -Archived $False | select GroupId;
 
-# Archiving Teams
+# Archiving Teams and locking respective SharePoint site.
 foreach($team in $teamsToArchive) {
     echo "Archiving team: $team.GroupId"
     Set-TeamArchivedState $team.GroupId -Archived $True -SetSpoSiteReadOnlyForMembers $True
